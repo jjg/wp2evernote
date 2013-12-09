29771c3b1a2c3782f754b9c3e94e870c25b46cb4
Jason J. Gullickson <jason.gullickson@gmail.com>
2013-12-09 00:57:16 -0600

    removed old test files

--- authtest.py
+++
@@ -1,7 +0,0 @@
-from evernote.api.client import EvernoteClient
-
-dev_token = 'S=s1:U=8d631:E=14a2adb30d1:C=142d32a04d4:P=1cd:A=en-devtoken:V=2:H=0dcc95fb7ef5991a98ec6c76605d53dd'
-client = EvernoteClient(token=dev_token)
-userStore = client.get_user_store()
-user = userStore.getUser()
-print user.username
