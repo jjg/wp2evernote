18c6acbbd4e49093d69224441662405c8687d6c0
Jason J. Gullickson <jason.gullickson@gmail.com>
2013-12-09 00:16:50 -0600

    you ever wait too long between commits?

--- wp2evernote.py
+++ wp2evernote.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python
 
 # wp2evernote.py
 # import Wordpress posts into Evernote as notes
@@ -12,10 +12,6 @@ import evernote.edam.userstore.constants as UserStoreConstants
 import evernote.edam.type.ttypes as Types
 from evernote.api.client import EvernoteClient
 import time
-
-
-# get the export file to process
-infile = sys.argv[1]
 
 
 def get_post(node):
@@ -66,18 +62,24 @@ def create_note(post):
 	# convert newlines to breaks
 	scrubbed_content = scrubbed_content.replace('\n', '<br/>')
 	
+	# close br's
+	scrubbed_content = scrubbed_content.replace('<br>', '<br/>')
+	
+	# close hr's
+	scrubbed_content = scrubbed_content.replace('<hr>', '<hr/>')
+	
 	# remove class directives (ie: class="size-medium wp-image-1756")
 	while scrubbed_content.find('class=') > 0:
 		
 		start_idx = scrubbed_content.find('class=')
-		print start_idx
+		#print start_idx
 		
 		end_idx = scrubbed_content.find('"', start_idx + 7)
-		print end_idx
+		#print end_idx
 		
 		scrubbed_content = scrubbed_content[:start_idx] + scrubbed_content[end_idx + 2:]
 		
-	# remove captions
+	# remove caption tags
 	while scrubbed_content.find('[caption') > 0:
 	
 		start_idx = scrubbed_content.find('[caption')
@@ -89,8 +91,6 @@ def create_note(post):
 		start_idx = scrubbed_content.find('[/caption]')
 		end_idx = start_idx + 10
 		scrubbed_content = scrubbed_content[:start_idx] + scrubbed_content[end_idx:]
-		
-	#scrubbed_content = scrubbed_content.replace('class=', 'ass=')
 	
 	# set the body
 	note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
@@ -108,32 +108,74 @@ def upload_note(note):
 	note = noteStore.createNote(note)
 	
 	
-# read posts from input file
-print 'reading %s' % infile
-dom = minidom.parse(infile)
 
-# process posts
-posts = []
-for node in dom.getElementsByTagName('item'):
-	post = get_post(node)
-	posts.append(post)
+if len(sys.argv) >= 2:
+
+	# get the export file to process
+	infile = sys.argv[1]
+
+	# read posts from input file
+	dom = minidom.parse(infile)
 	
-print 'processing %d posts' % len(posts)
-
-# generate notes
-notes = []
-for post in posts:
-	notes.append(create_note(post))
+	# process posts
+	posts = []
+	for node in dom.getElementsByTagName('item'):
+		post = get_post(node)
+		posts.append(post)
+		
+	print 'processing %d posts' % len(posts)
 	
-# debug
-print notes[517]
-#print notes[11]
-
-# try uploading one
-upload_note(notes[517])
-
-
-
-
-
-
+	# generate notes
+	notes = []
+	for post in posts:
+		notes.append(create_note(post))
+		
+		
+	# upload notes	
+	# resume if post # is specified
+	starting_post = 0
+	if len(sys.argv) > 2:
+		starting_post = int(sys.argv[2])
+		
+	# stop early if specified
+	ending_post = len(notes)
+	if len(sys.argv) > 3:
+		ending_post = int(sys.argv[3])
+	
+	if starting_post != 0:
+		print 'starting at post %d' % starting_post
+		
+	# header
+	print('post\tstatus')
+	
+	note_idx = 0
+	upload_errors = 0
+	for note in notes:
+		
+		if note_idx >= starting_post:
+			sys.stdout.write('%d' % note_idx)
+			
+			try:
+				upload_note(note)
+				sys.stdout.write('\tcomplete\n')
+				
+			except:
+				sys.stdout.write('\tfailed\n')
+				#print note
+				#print sys.exc_info()
+				#print 'error uploading note %d' % note_idx
+				upload_errors += 1
+				#sys.exit(1)
+			
+		if note_idx == ending_post:
+			break
+			
+		note_idx += 1
+	
+	total_uploaded = ((ending_post - starting_post) - upload_errors) + 1
+	print '%d posts uploaded' % total_uploaded
+	print '%d upload errors' % upload_errors
+	
+else:
+	
+	print 'usage: wp2evernote <wordpress export.xml> <starting post> <ending post>\n'
